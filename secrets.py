9693a0927164e88c264cddfcb67a4eaf49a8d0de
Jesus Manuel Ramirez Obeso <jesus.ramirez@wizeline.com>
2021-08-24 12:46:09 -0700

    Removing secrets

--- secrets.py
+++ secrets.py
@@ -5,10 +5,5 @@ ssh-private_key = "-----BEGIN OPENSSH PRIVATE KEY-----"
 
 secret_key = "EAtVLIW9qoHOvBP2rW5IpzNXt2BQC3UbTWmrprBpaUP8l9lx4OR"
 
-he_password = "asfiasdofnmsdlfgnmaslgvnmasdlgvnsdfoginsdfoginsdgon"
-
-rsa_key = "-----BEGIN RSA PRIVATE KEY-----"
-
-aws_key2 = "AKIAIOSFODNN7EXAMPLO"
 
 aws_key3 = "AKIAIOSFODNN5EXAMPLO"
