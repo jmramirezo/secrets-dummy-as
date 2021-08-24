c8408336582243aff7a3f07319e7681acf82d30c
Jesus Manuel Ramirez Obeso <jesus.ramirez@wizeline.com>
2021-08-24 12:36:29 -0700

    Added secrets

--- secrets.py
+++ secrets.py
@@ -1,1 +1,13 @@
+aws_key = "AKIAIOSFODNN7EXAMPLE"
+
+ssh-private_key = "-----BEGIN OPENSSH PRIVATE KEY-----"
+
+
+secret_key = "EAtVLIW9qoHOvBP2rW5IpzNXt2BQC3UbTWmrprBpaUP8l9lx4OR"
+
+he_password = "asfiasdofnmsdlfgnmaslgvnmasdlgvnsdfoginsdfoginsdgon"
+
+rsa_key = "-----BEGIN RSA PRIVATE KEY-----"
+
+aws_key2 = "AKIAIOSFODNN7EXAMPLO"
 
